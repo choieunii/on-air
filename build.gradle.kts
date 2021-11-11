71ff51607cde78df9fc6670da5bf8a2bf4743ce2
choieunii <ghk784@dgu.ac.kr>
2021-11-11 22:03:00 +0900

    Fix : Controller에서 .jsp로 이동되도록 설정 변경

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,6 +22,8 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	implementation("javax.servlet:jstl")
+	implementation("org.apache.tomcat.embed:tomcat-embed-jasper")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
--- src/main/kotlin/com/example/onair/controller/TestController.kt
+++ src/main/kotlin/com/example/onair/controller/TestController.kt
@@ -2,13 +2,13 @@ package com.example.onair.controller
 
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
-@RestController
-class ContentsController {
-    @GetMapping("/")
+@Controller
+class TestController {
+    @RequestMapping("/hello")
     fun index(): String {
-        val hello = "Hello World!"
-        return hello
+        return "hello";
     }
 }
